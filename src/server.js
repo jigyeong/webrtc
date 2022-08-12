b3125ad4a85eed4ce74301081343a4d938119b67
jigyeong <wlrud0126@gmail.com>
2022-08-12 17:25:09 +0900

    Frontend Setup

--- src/server.js
+++ src/server.js
@@ -3,9 +3,11 @@ import express from "express";
 const app = express();
 app.set("view engine", "pug");
 app.set("views", __dirname + "/views");
+app.use("/public", express.static(__dirname + "/public"))
 
-//route
+//router
 app.get("/", (req, res) => res.render("home"));
+app.get("/*", (req, res) => res.redirect("/"));
 
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
 
