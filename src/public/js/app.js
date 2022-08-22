8811c689aeb07b516fc654863a260087c5f82af4
jigyeong <wlrud0126@gmail.com>
2022-08-22 19:15:54 +0900

    Feat : Left Room & Show Nickname

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -14,11 +14,32 @@ function addMessage(msg){
     ul.appendChild(li);
 }
 
+function handleMessageSubmit(event){
+    event.preventDefault();
+    const input = room.querySelector("#msg input")
+    const value = input.value;
+    socket.emit("new_message", input.value, roomName, () => {
+        addMessage(`You : ${value}`)
+    })
+    input.value = "";
+}
+function handleNicknameSubmit(event){
+    event.preventDefault();
+    const input = room.querySelector("#name input")
+    const value = input.value;
+    socket.emit("nickname", input.value)
+    input.value = "";
+}
+
 function showRoom(msg) {
     welcome.hidden = true;
     room.hidden = false;
     const h3 = room.querySelector("h3");
     h3.innerText = `Room ${roomName}`;
+    const msgForm = room.querySelector("#msg");
+    const nameForm = room.querySelector("#name");
+    msgForm.addEventListener("submit", handleMessageSubmit)
+    nameForm.addEventListener("submit", handleNicknameSubmit)
 }
 
 
@@ -31,6 +52,12 @@ function handleRoomSubmit(event){
 }
 form.addEventListener("submit", handleRoomSubmit)
 
-socket.on("welcome", () => {
-    addMessage("Someone joined")
+socket.on("welcome", (user) => {
+    addMessage(`${user} joined`)
 })
+
+socket.on("bye", (left) => {
+    addMessage(`${left} left`)
+})
+
+socket.on("new_message",addMessage)
--- src/server.js
+++ src/server.js
@@ -17,7 +17,7 @@ const httpServer = http.createServer(app);
 const wsServer = new Server(httpServer);
 
 wsServer.on("connection", (socket) => {
-
+    socket["nickname"] = "anonymous"
     socket.onAny((event)=>{
         console.log(`Socket Event : ${event}`);
     })
@@ -25,8 +25,16 @@ wsServer.on("connection", (socket) => {
         socket.join(msg.payload)
         console.log(socket.rooms) // See all connections
         done();
-        socket.to(msg.payload).emit("welcome") // Send Except me
+        socket.to(msg.payload).emit("welcome", socket.nickname) // Send Except me
     })
+    socket.on("disconnecting", () => {
+        socket.rooms.forEach(room => socket.to(room).emit("bye",socket.nickname))
+    })
+    socket.on("new_message", (msg, room, done)=>{
+        socket.to(room).emit("new_message",`${socket.nickname} : ${msg}`);
+        done();
+    })
+    socket.on("nickname", (nickname)=> socket["nickname"]=nickname)
 })
 
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
