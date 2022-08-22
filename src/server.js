d9764927ed5e3a13d3912f01a614bcb6237822ec
jigyeong <wlrud0126@gmail.com>
2022-08-22 20:41:26 +0900

    Feat : Get PublickRooms

--- src/server.js
+++ src/server.js
@@ -16,6 +16,21 @@ app.get("/*", (req, res) => res.redirect("/"));
 const httpServer = http.createServer(app);
 const wsServer = new Server(httpServer);
 
+function publicRooms(){
+    const {
+        sockets : { 
+            adapter: { sids, rooms }
+        }
+    } = wsServer;
+    const publicRooms = [];
+    rooms.forEach((_, key)=> {
+       if(sids.get(key) === undefined){
+        publicRooms.push(key);
+       }
+    })
+    return publicRooms;
+}
+
 wsServer.on("connection", (socket) => {
     socket["nickname"] = "anonymous"
     socket.onAny((event)=>{
