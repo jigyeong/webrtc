7cb09f7da55d1fc6d5cfa9c8597c0455be9231df
jigyeong <wlrud0126@gmail.com>
2022-08-29 15:22:06 +0900

    Feat : Create RTCPeerConnection Offer

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -11,6 +11,7 @@ let myStream = null;
 let audioMuted = false;
 let cameraMuted = false;
 let roomName;
+let myPeerConnection;
 
 async function getCameras(){
     try{
@@ -86,10 +87,11 @@ cameraSelect.addEventListener("input", handleCameraChange)
 const welcome = document.getElementById("welcome")
 const welcomeForm = welcome.querySelector("form")
 
-function startMedia(){
+async function startMedia(){
     welcome.hidden = true;
     call.hidden = false;
-    getMedia();
+    await getMedia();
+    makeConnection();
 }
 
 function handleWelcomeSubmit(event) {
@@ -103,6 +105,21 @@ function handleWelcomeSubmit(event) {
 welcomeForm.addEventListener("submit", handleWelcomeSubmit)
 
 // Socket Code
-socket.on("welcome", ()=> {
-    console.log("someone joined")
+socket.on("welcome", async ()=> {
+    const offer = await myPeerConnection.createOffer();
+    myPeerConnection.setLocalDescription(offer)
+    console.log("sent the offer", offer)
+    socket.emit("offer", offer, roomName)
+});
+
+socket.on("offer",offer=>{
+    console.log(offer)
 })
+
+// RTC Code
+function makeConnection(){
+    myPeerConnection = new RTCPeerConnection();
+    myStream.getTracks().forEach(track => {
+        myPeerConnection.addTrack(track, myStream)
+    })
+}
